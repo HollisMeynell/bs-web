2dd521e692916a843bff8f49adc7ee4db4067601
Your Name <365246692@qq.com>
2023-09-16 17:04:47 +0800

    update

--- src/api/pool-group-api.js
+++ src/api/pool-group-api.js
@@ -1,5 +1,15 @@
 import {HttpRequest} from "@/api/util.js";
 
+/**
+ *
+ * @param {{
+ *     name: string,
+ *     info: string,
+ *     poolId: number,
+ *     color: number,
+ * }} data
+ * @returns {Promise<{}>}
+ */
 async function createGroup(data) {
     return await HttpRequest.put("/api/pool/", data);
 }
